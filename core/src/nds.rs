fa1d08611c86d0ce3e45c8d9da2cf450223eb54d
Akash Munagala <akash.munagala@gmail.com>
2020-07-07 21:39:25 -0700

    Synchronize between ARM9 and ARM7

--- core/src/nds.rs
+++ core/src/nds.rs
@@ -3,6 +3,7 @@ use crate::arm9::ARM9;
 use crate::hw::HW;
 
 pub struct NDS {
+    arm7_cycles_ahead: i32, // Measured in 33 MHz ARM7 cycles
     arm7: ARM7,
     arm9: ARM9,
     hw: HW,
@@ -12,6 +13,7 @@ impl NDS {
     pub fn new() -> Self {
         let mut hw = HW::new();
         NDS {
+            arm7_cycles_ahead: 0,
             arm7: ARM7::new(false, &mut hw),
             arm9: ARM9::new(false, &mut hw),
             hw,
@@ -19,6 +21,9 @@ impl NDS {
     }
 
     pub fn emulate_frame(&mut self) {
-        self.arm7.emulate_instr(&mut self.hw);
+        self.arm7_cycles_ahead += 2 * self.arm9.emulate_instr(&mut self.hw) as i32;
+        while self.arm7_cycles_ahead >= 0 {
+            self.arm7_cycles_ahead -= self.arm7.emulate_instr(&mut self.hw) as i32;
+        }
     }
 }
