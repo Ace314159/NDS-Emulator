7f5f29ccaab2928e1c44bf88f5a318efc1e64494
Akash Munagala <akash.munagala@gmail.com>
2020-07-08 22:33:51 -0700

    Trace ARM7 and ARM9 instructions to separate files

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     std::env::set_current_dir("ROMs").unwrap();
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Error, Config::default(), TerminalMode::Mixed),
-        TermLogger::new(LevelFilter::Off,
+        WriteLogger::new(LevelFilter::Off,
             ConfigBuilder::new()
             .set_time_level(LevelFilter::Off)
             .set_thread_level(LevelFilter::Off)
@@ -17,7 +17,18 @@ fn main() {
             .set_max_level(LevelFilter::Off)
             .add_filter_allow_str("nds_core::arm7")
             .build(),
-            TerminalMode::Mixed),
+        fs::File::create("arm7.log").unwrap()),
+        WriteLogger::new(LevelFilter::Off,
+            ConfigBuilder::new()
+            .set_time_level(LevelFilter::Off)
+            .set_thread_level(LevelFilter::Off)
+            .set_target_level(LevelFilter::Off)
+            .set_location_level(LevelFilter::Off)
+            .set_time_level(LevelFilter::Off)
+            .set_max_level(LevelFilter::Off)
+            .add_filter_allow_str("nds_core::arm9")
+            .build(),
+        fs::File::create("arm9.log").unwrap()),
     ]).unwrap();
     
     let bios7 = fs::read("bios7.bin").unwrap();
