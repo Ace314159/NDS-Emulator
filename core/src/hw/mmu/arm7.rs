f43ea8465709233fb8985e91e65d2d70f1820436
Akash Munagala <akash.munagala@gmail.com>
2020-07-08 18:27:06 -0700

    Setup memory regions for ARM7 and ARM9

--- core/src/hw/mmu/arm7.rs
+++ core/src/hw/mmu/arm7.rs
@@ -1,15 +1,47 @@
 use super::{AccessType, HW, MemoryValue};
 
+type MemoryRegion = ARM7MemoryRegion;
+
 impl HW {
     pub fn arm7_read<T: MemoryValue>(&self, addr: u32) -> T {
-        todo!()
+        match MemoryRegion::from_addr(addr) {
+            MemoryRegion::BIOS => todo!(),
+            MemoryRegion::MainMem => todo!(),
+            MemoryRegion::WRAM => todo!(),
+            MemoryRegion::IO => todo!(), 
+        }
     }
 
     pub fn arm7_write<T: MemoryValue>(&mut self, addr: u32, value: T) {
-        todo!()
+        match MemoryRegion::from_addr(addr) {
+            MemoryRegion::BIOS => todo!(),
+            MemoryRegion::MainMem => todo!(),
+            MemoryRegion::WRAM => todo!(),
+            MemoryRegion::IO => todo!(), 
+        }
     }
 
     pub fn arm7_get_access_time<T: MemoryValue>(&mut self, access_type: AccessType, addr: u32) -> usize {
         todo!()
+    }
+}
+
+pub enum ARM7MemoryRegion {
+    BIOS,
+    MainMem,
+    WRAM,
+    IO,
+}
+
+impl ARM7MemoryRegion {
+    pub fn from_addr(addr: u32) -> Self {
+        use ARM7MemoryRegion::*;
+        match addr >> 24 {
+            0x0 => BIOS,
+            0x2 => MainMem,
+            0x3 => WRAM,
+            0x4 => IO,
+            _ => todo!(),
+        }
     }
 }
--- core/src/hw/mmu/arm9.rs
+++ core/src/hw/mmu/arm9.rs
@@ -1,15 +1,70 @@
 use super::{AccessType, HW, MemoryValue};
 
+type MemoryRegion = ARM9MemoryRegion;
+
 impl HW {
     pub fn arm9_read<T: MemoryValue>(&self, addr: u32) -> T {
-        todo!()
+        match MemoryRegion::from_addr(addr) {
+            MemoryRegion::MainMem => todo!(),
+            MemoryRegion::WRAM => todo!(),
+            MemoryRegion::SharedWRAM => todo!(),
+            MemoryRegion::IO => todo!(),
+            MemoryRegion::Palette => todo!(),
+            MemoryRegion::VRAM => todo!(),
+            MemoryRegion::OAM => todo!(),
+            MemoryRegion::GBAROM => todo!(),
+            MemoryRegion::GBARAM => todo!(),
+            MemoryRegion::BIOS => todo!(),
+        }
     }
 
     pub fn arm9_write<T: MemoryValue>(&mut self, addr: u32, value: T) {
-        todo!()
+        match MemoryRegion::from_addr(addr) {
+            MemoryRegion::MainMem => todo!(),
+            MemoryRegion::WRAM => todo!(),
+            MemoryRegion::SharedWRAM => todo!(),
+            MemoryRegion::IO => todo!(),
+            MemoryRegion::Palette => todo!(),
+            MemoryRegion::VRAM => todo!(),
+            MemoryRegion::OAM => todo!(),
+            MemoryRegion::GBAROM => todo!(),
+            MemoryRegion::GBARAM => todo!(),
+            MemoryRegion::BIOS => todo!(),
+        }
     }
 
     pub fn arm9_get_access_time<T: MemoryValue>(&mut self, access_type: AccessType, addr: u32) -> usize {
         todo!()
+    }
+}
+
+pub enum ARM9MemoryRegion {
+    MainMem,
+    WRAM,
+    SharedWRAM,
+    IO,
+    Palette,
+    VRAM,
+    OAM,
+    GBAROM,
+    GBARAM,
+    BIOS,
+}
+
+impl ARM9MemoryRegion {
+    pub fn from_addr(addr: u32) -> Self {
+        use ARM9MemoryRegion::*;
+        match addr >> 24 {
+            0x2 => MainMem,
+            0x3 => WRAM,
+            0x4 => IO,
+            0x5 => Palette,
+            0x6 => VRAM,
+            0x7 => OAM,
+            0x8 | 0x9 => GBAROM,
+            0xA => GBARAM,
+            0xF if addr >> 16 == 0xFFFF => BIOS,
+            _ => todo!(),
+        }
     }
 }
