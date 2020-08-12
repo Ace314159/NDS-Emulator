35bff22fd0067c5ad63573c07b27a0d74552d02b
Akash Munagala <akash.munagala@gmail.com>
2020-08-12 13:34:55 -0700

    Fix inaccurate DMA count
    Lower 5 bits of DMACNT_H are upper 5 bits of NDS9 DMA count

--- core/src/hw/dma.rs
+++ core/src/hw/dma.rs
@@ -35,7 +35,6 @@ pub struct DMAChannel {
     pub cnt: DMACNT,
     sad: Address,
     dad: Address,
-    count: Count,
 }
 
 impl DMAChannel {
@@ -50,15 +49,14 @@ impl DMAChannel {
             cnt: DMACNT::new(is_nds9, num),
             sad: Address::new(if is_nds9 { 0x0FFF_FFFF } else { if num == 0 { 0x07FF_FFFF } else { 0x0FFF_FFFF} }),
             dad: Address::new(if is_nds9 { 0x0FFF_FFFF } else { if num == 3 { 0x07FF_FFFF } else { 0x0FFF_FFFF} }),
-            count: Count::new(if is_nds9 { 0x1F_FFFF } else { if num == 3 { 0xFFFF } else { 0x3FFF }}),
         }
     }
 
     pub fn latch(&mut self) {
         self.sad_latch = self.sad.addr & self.sad.mask;
         self.dad_latch = self.dad.addr & self.sad.mask;
-        let count = self.count.count & self.count.mask;
-        self.count_latch = if count == 0 { self.count.mask + 1 } else { count as u32 };
+        let count = self.cnt.count & self.cnt.count_mask;
+        self.count_latch = if count == 0 { self.cnt.count_mask + 1 } else { count };
     }
 }
 
@@ -73,10 +71,10 @@ impl IORegister for DMAChannel {
             0x5 => self.dad.read(1),
             0x6 => self.dad.read(2),
             0x7 => self.dad.read(3),
-            0x8 => self.count.read(0),
-            0x9 => self.count.read(1),
-            0xA => self.cnt.read(0),
-            0xB => self.cnt.read(1),
+            0x8 => self.cnt.read(0),
+            0x9 => self.cnt.read(1),
+            0xA => self.cnt.read(2),
+            0xB => self.cnt.read(3),
             _ => unreachable!(),
         }
     }
@@ -91,16 +89,16 @@ impl IORegister for DMAChannel {
             0x5 => self.dad.write(scheduler, 1, value),
             0x6 => self.dad.write(scheduler, 2, value),
             0x7 => self.dad.write(scheduler, 3, value),
-            0x8 => self.count.write(scheduler, 0, value),
-            0x9 => self.count.write(scheduler, 1, value),
-            0xA => self.cnt.write(scheduler, 0, value),
+            0x8 => self.cnt.write(scheduler, 0, value),
+            0x9 => self.cnt.write(scheduler, 1, value),
+            0xA => self.cnt.write(scheduler, 2, value),
             0xB => {
                 let prev_enable = self.cnt.enable;
-                self.cnt.write(scheduler, 1, value);
+                self.cnt.write(scheduler, 3, value);
                 if !prev_enable && self.cnt.enable {
                     self.latch();
                     info!("Scheduled {:?} ARM{} DMA{}: Writing {} values to {:08X} from {:08X}, size: {}",
-                    self.cnt.start_timing, if self.is_nds9 { 9 } else { 7 }, self.num, self.count.count,
+                    self.cnt.start_timing, if self.is_nds9 { 9 } else { 7 }, self.num, self.cnt.count,
                     self.dad.addr, self.sad.addr, if self.cnt.transfer_32 { 32 } else { 16 });
                     if self.cnt.start_timing == DMAOccasion::Immediate {
                         scheduler.run_now(Event::DMA(self.is_nds9, self.num))
@@ -154,6 +152,8 @@ impl DMAOccasion {
 
 
 pub struct DMACNT {
+    count: u32,
+    pub count_latch: u32,
     pub dest_addr_ctrl: u8,
     pub src_addr_ctrl: u8,
     pub repeat: bool,
@@ -164,11 +164,14 @@ pub struct DMACNT {
 
     is_nds9: bool,
     num: usize,
+    count_mask: u32,
 }
 
 impl DMACNT {
     pub fn new(is_nds9: bool, num: usize) -> Self {
         DMACNT {
+            count: 0,
+            count_latch: 0,
             dest_addr_ctrl: 0,
             src_addr_ctrl: 0,
             repeat: false,
@@ -179,6 +182,7 @@ impl DMACNT {
 
             is_nds9,
             num,
+            count_mask: if is_nds9 { 0x1F_FFFF } else { if num == 3 { 0xFFFF } else { 0x3FFF }},
         }
     }
 }
@@ -186,8 +190,9 @@ impl DMACNT {
 impl IORegister for DMACNT {
     fn read(&self, byte: usize) -> u8 {
         match byte {
-            0 => (self.src_addr_ctrl & 0x1) << 7 | self.dest_addr_ctrl << 5,
-            1 => (self.enable as u8) << 7 | (self.irq as u8) << 6 | (self.start_timing as u8) << 3 |
+            0 | 1 => HW::read_byte_from_value(&self.count, byte),
+            2 => (self.src_addr_ctrl & 0x1) << 7 | self.dest_addr_ctrl << 5 | (self.count >> 16) as u8,
+            3 => (self.enable as u8) << 7 | (self.irq as u8) << 6 | (self.start_timing as u8) << 3 |
                 (self.transfer_32 as u8) << 2 | (self.repeat as u8) << 1 | self.src_addr_ctrl >> 1,
             _ => unreachable!(),
         }
@@ -195,11 +200,13 @@ impl IORegister for DMACNT {
 
     fn write(&mut self, _scheduler: &mut Scheduler, byte: usize, value: u8) {
         match byte {
-            0 => {
+            0 | 1 => HW::write_byte_to_value(&mut self.count, byte, value),
+            2 => {
+                if self.is_nds9 { HW::write_byte_to_value(&mut self.count, 2, value & 0x1F)}
                 self.src_addr_ctrl = self.src_addr_ctrl & !0x1 | value >> 7 & 0x1;
                 self.dest_addr_ctrl = value >> 5 & 0x3;
             },
-            1 => {
+            3 => {
                 self.enable = value >> 7 & 0x1 != 0;
                 self.irq = value >> 6 & 0x1 != 0;
                 self.start_timing = DMAOccasion::get(self.is_nds9, self.num, value >> 3 & 0x7);
@@ -232,27 +239,5 @@ impl IORegister for Address {
 
     fn write(&mut self, _scheduler: &mut Scheduler, byte: usize, value: u8) {
         HW::write_byte_to_value(&mut self.addr, byte, value);
-    }
-}
-
-pub struct Count {
-    pub count: u32,
-    mask: u32,
-}
-
-impl Count {
-    pub fn new(mask: u32) -> Count {
-        Count {
-            count: 0,
-            mask,
-        }
-    }
-}
-
-impl IORegister for Count {
-    fn read(&self, byte: usize) -> u8 { HW::read_byte_from_value(&self.count, byte) }
-
-    fn write(&mut self, _scheduler: &mut Scheduler, byte: usize, value: u8) {
-        HW::write_byte_to_value(&mut self.count, byte, value);
     }
 }
