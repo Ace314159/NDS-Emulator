3063f47255db98b70497c448ef59dd95dc751381
Akash Munagala <akash.munagala@gmail.com>
2020-08-13 22:12:41 -0700

    Fix typos with Modes 3-5

--- core/src/hw/gpu/engine2d.rs
+++ core/src/hw/gpu/engine2d.rs
@@ -128,40 +128,40 @@ impl<E: EngineType> Engine2D<E> {
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_text_line(vram, vcount, 2) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_affine_line(vram, 2) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_affine_line(vram, 3) }
                 self.process_lines(vcount, 0, 3);
             },
             BGMode::Mode2 => {
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 3) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 3) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_affine_line(vram, 2) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_affine_line(vram, 3) }
                 self.process_lines(vcount, 0, 3);
             },
             BGMode::Mode3 => {
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 3) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 3) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_text_line(vram, vcount, 2) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_extended_line(vram, vcount, 3) }
                 self.process_lines(vcount, 0, 3);
             },
             BGMode::Mode4 => {
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 3) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 3) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_affine_line(vram, 2) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_extended_line(vram, vcount, 3) }
                 self.process_lines(vcount, 0, 3);
             },
             BGMode::Mode5 => {
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 3) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 3) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_extended_line(vram, vcount, 2) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_extended_line(vram, vcount, 3) }
                 self.process_lines(vcount, 0, 3);
             },
             BGMode::Mode6 => {
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 3) }
-                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 3) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG0) { self.render_text_line(vram, vcount, 0) }
+                if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG1) { self.render_text_line(vram, vcount, 1) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG2) { self.render_extended_line(vram, vcount, 2) }
                 if self.dispcnt.contains(DISPCNTFlags::DISPLAY_BG3) { self.render_extended_line(vram, vcount, 3) }
                 self.process_lines(vcount, 0, 3);
