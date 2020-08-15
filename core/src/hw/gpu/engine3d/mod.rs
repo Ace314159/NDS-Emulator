1cfa65267caa93a7fcd4f5077948a388171a79e9
Akash Munagala <akash.munagala@gmail.com>
2020-08-15 00:16:17 -0700

    Implement reading and writing GXSTAT

--- core/src/hw/gpu/engine3d/mod.rs
+++ core/src/hw/gpu/engine3d/mod.rs
@@ -1,15 +1,18 @@
+use crate::hw::mmu::IORegister;
 use super::{GPU, Scheduler};
 
 mod registers;
 
-pub struct Engine3D {
+use registers::*;
 
+pub struct Engine3D {
+    gxstat: GXSTAT,
 }
 
 impl Engine3D {
     pub fn new() -> Self {
         Engine3D {
-
+            gxstat: GXSTAT::new(),
         }
     }
 
@@ -23,13 +26,15 @@ impl Engine3D {
     pub fn read_register(&self, addr: u32) -> u8 {
         assert_eq!(addr >> 12, 0x04000);
         match addr & 0xFFF {
+            0x600 ..= 0x603 => self.gxstat.read((addr as usize) & 0x3),
             _ => { warn!("Ignoring Engine3D Read at 0x{:08X}", addr); 0 },
         }
     }
 
-    pub fn write_register(&mut self, _scheduler: &mut Scheduler, addr: u32, value: u8) {
+    pub fn write_register(&mut self, scheduler: &mut Scheduler, addr: u32, value: u8) {
         assert_eq!(addr >> 12, 0x04000);
         match addr & 0xFFF {
+            0x600 ..= 0x603 => self.gxstat.write(scheduler, (addr as usize) & 0x3, value),
             _ => warn!("Ignoring Engine3D Write 0x{:08X} = {:02X}", addr, value),
         }
     }
