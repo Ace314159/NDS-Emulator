fa71fb9febbe874d18a0fa0bb8f7d8cf43f464b6
Akash Munagala <akash.munagala@gmail.com>
2020-12-05 01:48:54 -0800

    FIx GXFIFO DMA overrun bug
    Keep track of words left and disable once none are left

--- core/src/hw/scheduler.rs
+++ core/src/hw/scheduler.rs
@@ -119,7 +119,10 @@ impl HW {
         // TODO: Optimize
         let count = if channel.cnt.start_timing == DMAOccasion::GeometryCommandFIFO {
             channel.cnt.enable = count > 112;
-            std::cmp::min(count, 112)
+            let cur_count = std::cmp::min(count, 112);
+            channel.count_latch -= cur_count;
+            channel.cnt.enable = channel.count_latch > 0;
+            cur_count
         } else { count };
         info!("Running {:?} ARM{} DMA{}: Writing {} values to {:08X} from {:08X}, size: {}", channel.cnt.start_timing,
         if is_nds9 { 9 } else { 7 }, num, count, dest_addr, src_addr, if transfer_32 { 32 } else { 16 });
